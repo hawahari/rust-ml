9d2584c13485e21bd1ccc4c005146f4dd516534c
hawahari <darlami939@gmail.com>
2025-02-24 13:21:41 +0545

    src folder

--- data-wrangler/src/main.rs
+++ data-wrangler/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use csv::Reader;
+fn main()->Result<(),Box<dyn Error>>{
+
+    let mut file=File::open("/home/legion/Downloads/new_test_set_with_ruminating (1).txt")?;
+    let mut reader=Reader::from_reader(file);
+    for result in reader.records(){
+    let record=result?;
+    println!("{:?}",record);
 }
+Ok(())
+}
+
